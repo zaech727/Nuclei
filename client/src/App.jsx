30b3fd4e5bf4381474e5b926e7486dbc97a0ebd4
laurnen <68237271+laurnen@users.noreply.github.com>
2024-04-06 16:56:12 -0500

    Installed modules

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="w-full p-6">
+      <div className="flex h-screen">
+        <Navbar />
+      </div>
+    </div>
+  );
+};
+export default App
