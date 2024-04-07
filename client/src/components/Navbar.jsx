dd5151f5cf490669def5b70e44f441baeef5ede4
Nicholas <nkpqiu@gmail.com>
2024-04-07 01:00:01 -0500

    Updating style + adding button functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,10 @@
 import logo from '../assets/logo.png';
+import { useState } from 'react';
 
-const Navbar = () => {
+const Navbar = ({ onChange }) => {
+
+    const [selected, setSelected] = useState('Dashboard');
+
     return (
             <div className="text-white w-64 flex-none">
                 <div className="flex flex-col justify-between h-full">
@@ -10,36 +14,36 @@ const Navbar = () => {
                     </div>
                     {/* Navigation */}
                     <nav className="flex flex-col flex-grow  text-offwhite ml-4">
-                        <button className="flex px-4 py-2.5 mt-2 bg-white bg-opacity-30 rounded-xl">
+                        <button className={`flex px-4 py-2.5 mt-2 rounded-xl ${selected === "Dashboard" ? `bg-white bg-opacity-30` : `opacity-50`}`} onClick={() => {onChange('Dashboard'); setSelected('Dashboard')}}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-8">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
                             </svg>
-                            <span className="font-bold">Events</span>
+                            <span className={selected === "Dashboard" ? `font-bold`: ``}>Events</span>
                             </button>
-                        <button className="flex px-4 py-2.5 mt-2 rounded-xl opacity-50">
+                        <button className={`flex px-4 py-2.5 mt-2 rounded-xl ${selected === "Graph" ? `bg-white bg-opacity-30` : `opacity-50`}`} onClick={() => {onChange('Graph'); setSelected('Graph')}}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-8">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
                             </svg>
 
-                            <span className="">Graph View</span>
+                            <span className={selected === "Graph" ? `font-bold`: ``}>Graph View</span>
                         </button>
-                        <button className="flex px-4 py-2.5 mt-2 rounded-xl opacity-50">
+                        <button className={`flex px-4 py-2.5 mt-2 rounded-xl ${selected === "Calendar" ? `bg-white bg-opacity-30` : `opacity-50`}`} onClick={() => {onChange("Calendar"); setSelected("Calendar")}}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-8">
   <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
 </svg>
 
-                        <span className="">Calendar</span>
+                        <span className={selected === "Calendar" ? `font-bold`: ``}>Calendar</span>
                         </button>
-                        <button className="flex px-4 py-2.5 mt-2 rounded-xl opacity-50">
+                        <button className={`flex px-4 py-2.5 mt-2 rounded-xl ${selected === "Members" ? `bg-white bg-opacity-30` : `opacity-50`}`} onClick={() => {onChange('Members'); setSelected('Members')}}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-8">
   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
 </svg>
 
-                        <span className="">Members</span>
+                        <span className={selected === "Members" ? `font-bold`: ``}>Members</span>
                         </button>
                         <span className="w-full h-4/6"></span>
-                        <button className="flex px-4 py-2 mb-10">
+                        <button className="flex px-4 py-2 mb-10" onClick={() => onChange('Settings')}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-12 mr-4">
   <path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
   <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
