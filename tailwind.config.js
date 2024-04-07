13240569101b83bae612b6eb951040320abe03ec
laurnen <68237271+laurnen@users.noreply.github.com>
2024-04-07 08:42:02 -0500

    Updated

---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "sidepanel.html",
+    "*.{html,js}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'offwhite': '#F9F5E3',
+      'primarypink': '#EF798A',
+      'secondarypink': '#D14081',
+      'primarypurple': '#5B2A86',
+      'secondarypurple': '#360568',
+      'white': '#ffffff',
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'black': '#000000',
+    },
+    extend: {
+      fontFamily: {
+        'riicookie': ['RiiCookie'],
+        'starflower': ['Starflower'],
+        'casual': ['Casual'],
+        'jost': ['Jost'],
+        'roboto': ['Roboto'],
+        'aldercy': ['Aldercy'],
+        'mistys': ['Mistys'],
+        'cogley': ['Cogley'],
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
+  },
+  plugins: [],
+}
